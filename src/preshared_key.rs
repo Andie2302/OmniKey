dd35bb54e04e9a267534cbb409ef79ea79c7be5b
andreas <Andreas.Zwander@gmail.com>
2026-04-30 21:58:42 +0200

    Implement `Display` and `From<Vec<u8>>` for `PresharedKey`

--- src/preshared_key.rs
+++ src/preshared_key.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use crate::key::Key;
 
 #[derive(Debug, Clone)]
@@ -8,5 +9,18 @@ pub struct PresharedKey {
 impl PresharedKey {
     pub fn new(preshared_key: Key) -> Self {
         Self { preshared_key }
+    }
+}
+
+impl fmt::Display for PresharedKey {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let value = self.preshared_key.to_base64();
+        write!(f, "  Value: {value}")
+    }
+}
+
+impl From<(Vec<u8>)> for PresharedKey {
+    fn from((bytes): (Vec<u8>)) -> Self {
+        Self::new(Key::from(bytes))
     }
 }
