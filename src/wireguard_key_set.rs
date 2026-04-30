0d4c4e3db8cc6b467499e3065492eec6906779fa
andreas <Andreas.Zwander@gmail.com>
2026-04-30 21:51:37 +0200

    Refactor `WireGuardKeySet`: replace `Key` with `PrivateKey`, `PublicKey`, and `PresharedKey` for improved type clarity

--- src/wireguard_key_set.rs
+++ src/wireguard_key_set.rs
@@ -1,20 +1,22 @@
-use crate::key::Key;
 use crate::key_set::KeySet;
+use crate::preshared_key::PresharedKey;
+use crate::private_key::PrivateKey;
+use crate::public_key::PublicKey;
 use std::fmt;
 
 #[derive(Debug, Clone)]
 pub struct WireGuardKeySet {
     keyset: KeySet,
-    preshared_key: Option<Key>,
+    preshared_key: Option<PresharedKey>,
 }
 
 impl WireGuardKeySet {
-    pub fn new(keyset: KeySet, preshared_key: Option<Key>) -> Self {
+    pub fn new(keyset: KeySet, preshared_key: Option<PresharedKey>) -> Self {
         Self { keyset, preshared_key }
     }
-    pub fn private_key(&self) -> &Key { self.keyset.private_key() }
-    pub fn public_key(&self) -> &Key  { self.keyset.public_key() }
-    pub fn preshared_key(&self) -> Option<&Key> {
+    pub fn private_key(&self) -> &PrivateKey { self.keyset.private_key() }
+    pub fn public_key(&self) -> &PublicKey  { self.keyset.public_key() }
+    pub fn preshared_key(&self) -> Option<&PresharedKey> {
         self.preshared_key.as_ref()
     }
 }
