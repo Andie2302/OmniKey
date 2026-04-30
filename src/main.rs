ac3642e1b19a5398b92d33a7e7d32d43a25946ce
andreas <Andreas.Zwander@gmail.com>
2026-04-30 20:55:14 +0200

    Update `main.rs` to include `WireGuardKeySet` usage and streamline key handling

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
 use OmniKey::key::Key;
-use OmniKey::key_set::{KeySet};
+use OmniKey::key_set::KeySet;
+use OmniKey::wireguard_key_set::WireGuardKeySet;
 
 fn main() -> std::io::Result<()> {
 
     let key1 = Key::from(vec![1, 2, 3]);
-    println!("{key1}");
-
     let key2 = Key::from(vec![4, 5, 6]);
-    println!("{key2}");
-
+    let preshared = Key::from(vec![7, 8, 9]);
     let keyset = KeySet::new(key1.clone(), key2.clone());
     println!("{keyset}");
-
-    let keyset2 = KeySet::new(key1, key2);
-    println!("{keyset2}");
-
+    let wireguard_key_set = WireGuardKeySet::new(keyset.clone(), Option::from(preshared.clone()));
+    println!("{wireguard_key_set}");
     Ok(())
 }
