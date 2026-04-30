cbd3dc442b846b11bf9c6971e2c3bdfdfcb5a4dc
andreas <Andreas.Zwander@gmail.com>
2026-04-30 02:41:42 +0200

    Remove outdated comments from `Generate` trait definition

--- src/generate.rs
+++ src/generate.rs
@@ -1,6 +1,3 @@
-/// Trait für alle Keyset-Generatoren.
-/// Implementiere diesen Trait für jeden Keyset-Typ,
-/// um eine einheitliche `generate()`-Schnittstelle zu erhalten.
 pub trait Generate: Sized {
     fn generate() -> Self;
 }
