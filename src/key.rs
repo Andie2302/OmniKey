523294ce31d87691a8429cae9817649b4a9e97d2
andreas <Andreas.Zwander@gmail.com>
2026-04-30 15:37:20 +0200

    Refactor `Key` struct: streamline `as_base64` implementation and clean up formatting.

--- src/key.rs
+++ src/key.rs
@@ -1,28 +1,23 @@
 use std::fmt;
 use base64::{engine::general_purpose::STANDARD, Engine};
-
 const BASE64_ENGINE: &'static base64::engine::general_purpose::GeneralPurpose = &STANDARD;
-
 #[derive(Debug, Clone)]
 pub struct Key {
     pub raw: Vec<u8>,
 }
-
 impl From<Vec<u8>> for Key {
     fn from(raw: Vec<u8>) -> Self {
         Self { raw }
     }
 }
-
 impl Key {
+    pub fn as_base64(&self) -> String {
+        BASE64_ENGINE.encode(&self.raw)
+    }
     pub fn as_bytes(&self) -> &[u8] {
         &self.raw
     }
-    pub fn as_base64(&self) -> String {
-        BASE64_ENGINE.encode(self.as_bytes())
-    }
 }
-
 impl fmt::Display for Key {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.as_base64())
