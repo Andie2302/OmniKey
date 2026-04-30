415300dc6ddae77ae04ab39fbfc24dbc5fa6eb85
andreas <Andreas.Zwander@gmail.com>
2026-04-30 20:19:39 +0200

    Simplify `main` by removing unused `KeySetType` references and streamlining `KeySet` initialization.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
 use OmniKey::key::Key;
-use OmniKey::keyset::{KeySet, KeySetType};
+use OmniKey::keyset::{KeySet};
 
 fn main() -> std::io::Result<()> {
-    // Variante 1: Mit Label (nutzt das Tupel-From)
-    let key1 = Key::from((vec![1, 2, 3], String::from("abc")));
+
+    let key1 = Key::from(vec![1, 2, 3]);
     println!("{key1}");
 
-    // Variante 2: Ohne Label (nutzt das einfache Vec-From)
     let key2 = Key::from(vec![4, 5, 6]);
     println!("{key2}");
 
-    let keyset = KeySet::new(key1.clone(), key2.clone(), KeySetType::Custom("Test".to_string()));
+    let keyset = KeySet::new(key1.clone(), key2.clone());
     println!("{keyset}");
 
-    let keyset2 = KeySet::new(key1, key2, Default::default() );
+    let keyset2 = KeySet::new(key1, key2);
     println!("{keyset2}");
 
     Ok(())
