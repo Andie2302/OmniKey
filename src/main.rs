a29715b23b8b1901a7e79b4498e4d6e870a80c5d
andreas <Andreas.Zwander@gmail.com>
2026-05-01 01:16:05 +0200

    Update `main.rs` to include WireGuard key generation with and without preshared keys

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
-use OmniKey::generator::KeyGenerator;
-use OmniKey::wireguard_key_set::WireGuardKeySet;
+use OmniKey::generator::{KeyGenerator, WireGuardOptions};
 use OmniKey::ssh_key_set::SSHKeySet;
+use OmniKey::wireguard_key_set::WireGuardKeySet;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let wg = WireGuardKeySet::generate()?;
-    println!("{wg}");
+    // SSH: Ed25519, OpenSSH-Format
     let ssh = SSHKeySet::generate()?;
     println!("{ssh}");
+
+    // WireGuard: ohne Preshared Key
+    let wg = WireGuardKeySet::generate()?;
+    println!("{wg}");
+
+    // WireGuard: mit Preshared Key
+    let wg_psk = WireGuardKeySet::generate_with(WireGuardOptions::with_preshared());
+    println!("{wg_psk}");
+
     Ok(())
 }
