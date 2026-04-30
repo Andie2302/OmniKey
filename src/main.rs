1a29331fe1629aa2746e4c969b74b698561fefbe
andreas <Andreas.Zwander@gmail.com>
2026-04-30 23:54:47 +0200

    Update `main.rs` to handle `WireGuardKeySet::generate` result and import `KeyGenerator`

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use OmniKey::generator::KeyGenerator;
 use OmniKey::wireguard_key_set::WireGuardKeySet;
 use OmniKey::ssh_key_set::SSHKeySet;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let wg = WireGuardKeySet::generate(true);
+    let wg = WireGuardKeySet::generate()?;
     println!("{wg}");
     let ssh = SSHKeySet::generate()?;
     println!("{ssh}");
