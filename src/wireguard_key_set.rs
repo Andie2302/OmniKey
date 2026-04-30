b759e7ad7e60fe5dbc8f64304b13dd88a737aed1
andreas <Andreas.Zwander@gmail.com>
2026-04-30 23:56:14 +0200

    Remove unnecessary blank line in `wireguard_key_set.rs` for cleaner code

--- src/wireguard_key_set.rs
+++ src/wireguard_key_set.rs
@@ -14,7 +14,6 @@ impl WireGuardKeySet {
     pub fn new(keyset: KeySet, preshared_key: Option<PresharedKey>) -> Self {
         Self { keyset, preshared_key }
     }
-    
     pub fn private_key(&self) -> &PrivateKey         { self.keyset.private_key() }
     pub fn public_key(&self)  -> &PublicKey          { self.keyset.public_key()  }
     pub fn preshared_key(&self) -> Option<&PresharedKey> { self.preshared_key.as_ref() }
