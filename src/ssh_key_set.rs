3a6b3f33c93dbbecb7be33643c1e2cd9d208d9a5
andreas <Andreas.Zwander@gmail.com>
2026-04-30 21:33:06 +0200

    Fix method calls in `SSHKeySet` to directly utilize `KeySet` methods

--- src/ssh_key_set.rs
+++ src/ssh_key_set.rs
@@ -9,6 +9,6 @@ impl SSHKeySet {
     pub fn new(keyset: KeySet) -> Self {
         Self { keyset }
     }
-    pub fn private_key(&self) -> &Key { &self.keyset.private_key() }
-    pub fn public_key(&self) -> &Key { &self.keyset.public_key() }
+    pub fn private_key(&self) -> &Key { self.keyset.private_key() }
+    pub fn public_key(&self) -> &Key { self.keyset.public_key() }
 }
