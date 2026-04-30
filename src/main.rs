242c5e9faf328a9664dcfdfb07f5347089973739
andreas <Andreas.Zwander@gmail.com>
2026-04-30 22:03:22 +0200

    Refactor `main.rs`: replace `Key` with `PublicKey`, `PrivateKey`, and `PresharedKey` to enhance type clarity

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use OmniKey::key::{Key};
 use OmniKey::key_set::KeySet;
 use OmniKey::wireguard_key_set::WireGuardKeySet;
+use OmniKey::public_key::PublicKey;
+use OmniKey::preshared_key::PresharedKey;
+use OmniKey::private_key::PrivateKey;
 
 fn main() -> std::io::Result<()> {
 
-    let public = Key::from(vec![1, 2, 3]);
-    let private = Key::from(vec![4, 5, 6]);
-    let preshared = Key::from(vec![7, 8, 9]);
+    let public = PublicKey::from(vec![1, 2, 3]);
+    let private = PrivateKey::from(vec![4, 5, 6]);
+    let preshared = PresharedKey::from(vec![7, 8, 9]);
     let keyset = KeySet::new(private.clone(), public.clone());
     println!("{keyset}");
     let wireguard_key_set = WireGuardKeySet::new(keyset.clone(), Option::from(preshared.clone()));
