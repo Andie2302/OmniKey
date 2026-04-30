652b4f062cd8518b5c3149d774a6628aabdf837d
andreas <Andreas.Zwander@gmail.com>
2026-04-30 21:58:57 +0200

    Implement `Display` and `From<Vec<u8>>` for `PublicKey` to enable formatted output and conversion

--- src/public_key.rs
+++ src/public_key.rs
@@ -1,12 +1,27 @@
+use std::fmt;
 use crate::key::Key;
+use crate::preshared_key::PresharedKey;
 
 #[derive(Debug, Clone)]
 pub struct PublicKey {
-    pub key: Key,
+    pub public_key: Key,
 }
 
 impl PublicKey {
     pub fn new(key: Key) -> Self {
-        Self { key }
+        Self { public_key: key }
+    }
+}
+
+impl fmt::Display for PublicKey {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let value = self.public_key.to_base64();
+        write!(f, "  Value: {value}")
+    }
+}
+
+impl From<(Vec<u8>)> for PublicKey {
+    fn from((bytes): (Vec<u8>)) -> Self {
+        Self::new(Key::from(bytes))
     }
 }
