4d3d76c6712b17ed9e806cf98d51e8cf7d753f59
andreas <Andreas.Zwander@gmail.com>
2026-04-30 21:58:46 +0200

    Implement `Display` for `PrivateKey` to format key output

--- src/private_key.rs
+++ src/private_key.rs
@@ -1,4 +1,6 @@
+use std::fmt;
 use crate::key::Key;
+use crate::preshared_key::PresharedKey;
 
 #[derive(Debug, Clone)]
 pub struct PrivateKey {
@@ -8,5 +10,12 @@ pub struct PrivateKey {
 impl PrivateKey {
     pub fn new(private_key: Key) -> Self {
         Self { private_key }
+    }
+}
+
+impl fmt::Display for PrivateKey {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let value = self.private_key.to_base64();
+        write!(f, "  Value: {value}")
     }
 }
