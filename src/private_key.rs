dbd6f29c6acc683e5606b472a69b55bf3ae56894
andreas <Andreas.Zwander@gmail.com>
2026-04-30 22:20:45 +0200

    Enhance `PrivateKey`: add `as_bytes` and `to_base64` methods, refine `Display` formatting, and fix `From<Vec<u8>>` implementation

--- src/private_key.rs
+++ src/private_key.rs
@@ -10,17 +10,24 @@ impl PrivateKey {
     pub fn new(private_key: Key) -> Self {
         Self { private_key }
     }
+
+    pub fn as_bytes(&self) -> &[u8] {
+        self.private_key.as_bytes()
+    }
+
+    pub fn to_base64(&self) -> String {
+        self.private_key.to_base64()
+    }
 }
 
 impl fmt::Display for PrivateKey {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let value = self.private_key.to_base64();
-        write!(f, "  Value: {value}")
+        write!(f, "  Value: {}", self.private_key.to_base64())
     }
 }
 
-impl From<(Vec<u8>)> for PrivateKey {
-    fn from((bytes): (Vec<u8>)) -> Self {
+impl From<Vec<u8>> for PrivateKey {
+    fn from(bytes: Vec<u8>) -> Self {
         Self::new(Key::from(bytes))
     }
 }
