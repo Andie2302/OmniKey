451a4cfd977aed625594d748115f1d32513b0750
andreas <Andreas.Zwander@gmail.com>
2026-04-30 20:55:20 +0200

    Refactor `WireGuardKeySet` to support optional preshared keys, implement `Deref` trait, and add `Display` formatting

--- src/wireguard_key_set.rs
+++ src/wireguard_key_set.rs
@@ -1,16 +1,40 @@
+use std::fmt;
+use std::ops::Deref;
 use crate::key::Key;
 use crate::key_set::KeySet;
 
+#[derive(Debug, Clone)]
 pub struct WireGuardKeySet {
     keyset: KeySet,
-    preshared_key: Key,
+    preshared_key: Option<Key>,
 }
 
 impl WireGuardKeySet {
-    pub fn new(keyset: KeySet, preshared_key: Key) -> Self {
+    pub fn new(keyset: KeySet, preshared_key: Option<Key>) -> Self {
         Self { keyset, preshared_key }
     }
-    pub fn private_key(&self) -> &Key { &self.keyset.private_key() }
-    pub fn public_key(&self) -> &Key { &self.keyset.public_key() }
-    pub fn preshared_key(&self) -> &Key { &self.preshared_key }
+
+    pub fn preshared_key(&self) -> Option<&Key> {
+        self.preshared_key.as_ref()
+    }
+}
+
+impl Deref for WireGuardKeySet {
+    type Target = KeySet;
+
+    fn deref(&self) -> &Self::Target {
+        &self.keyset
+    }
+}
+
+impl fmt::Display for WireGuardKeySet {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "WireGuard KeySet:")?;
+        write!(f, "{}", self.keyset)?;
+        if let Some(key) = &self.preshared_key {
+            write!(f, "  Preshared: {}", key)?;
+        }
+        writeln!(f,"")?;
+        Ok(())
+    }
 }
