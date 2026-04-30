1a80cf64bc1213e2350aebe0d31342cfd5686967
andreas <Andreas.Zwander@gmail.com>
2026-04-30 20:07:27 +0200

    Remove extra blank lines in `main.rs` for improved readability.

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,11 @@ fn main() -> std::io::Result<()> {
     let key2 = Key::from(vec![4, 5, 6]);
     println!("{key2}");
 
-
     let keyset = KeySet::new(key1.clone(), key2.clone(), KeySetType::Custom("Test".to_string()));
     println!("{keyset}");
 
     let keyset2 = KeySet::new(key1, key2, Default::default() );
     println!("{keyset2}");
-
 
     Ok(())
 }
