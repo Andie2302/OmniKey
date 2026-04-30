66550c68c012c95372f3c44c6b546a94bdff253c
andreas <Andreas.Zwander@gmail.com>
2026-04-30 21:51:32 +0200

    Refactor `SSHKeySet`: replace `Key` with `PrivateKey` and `PublicKey` for improved type clarity

--- src/ssh_key_set.rs
+++ src/ssh_key_set.rs
@@ -1,5 +1,6 @@
-use crate::key::Key;
 use crate::key_set::KeySet;
+use crate::private_key::PrivateKey;
+use crate::public_key::PublicKey;
 
 pub struct SSHKeySet {
     keyset: KeySet,
@@ -9,6 +10,6 @@ impl SSHKeySet {
     pub fn new(keyset: KeySet) -> Self {
         Self { keyset }
     }
-    pub fn private_key(&self) -> &Key { self.keyset.private_key() }
-    pub fn public_key(&self) -> &Key { self.keyset.public_key() }
+    pub fn private_key(&self) -> &PrivateKey { self.keyset.private_key() }
+    pub fn public_key(&self) -> &PublicKey { self.keyset.public_key() }
 }
