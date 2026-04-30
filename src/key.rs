fe4be80834ac8a87a6a8009f90c613b9bd0f63e2
andreas <Andreas.Zwander@gmail.com>
2026-04-30 21:51:03 +0200

    Refactor `Key` struct: remove `KeyType`, simplify constructor, and update `Display` implementation

--- src/key.rs
+++ src/key.rs
@@ -2,31 +2,15 @@ use std::fmt;
 use base64::Engine;
 use base64::engine::general_purpose::STANDARD;
 
-#[derive(Debug, Clone)]
-pub enum KeyType {
-    Public,
-    Private,
-    Preshared
-}
-impl fmt::Display for KeyType {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            KeyType::Public => write!(f, "Public"),
-            KeyType::Private => write!(f, "Private"),
-            KeyType::Preshared => write!(f, "Preshared")
-        }
-    }
-}
 
 #[derive(Debug, Clone)]
 pub struct Key {
     bytes: Vec<u8>,
-    key_type: KeyType,
 }
 
 impl Key {
-    pub fn new(bytes: Vec<u8>, key_type: KeyType) -> Self {
-        Self { bytes, key_type }
+    pub fn new(bytes: Vec<u8>) -> Self {
+        Self { bytes}
     }
 
     pub fn as_bytes(&self) -> &[u8] {
@@ -40,16 +24,14 @@ impl Key {
 
 impl fmt::Display for Key {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let k_type = &self.key_type;
         let value = self.to_base64();
-        writeln!(f, "  Type: {k_type}")?;
         write!(f, "  Value: {value}")
     }
 }
 
-impl From<(Vec<u8>, KeyType)> for Key {
-    fn from((bytes, k_type): (Vec<u8>, KeyType)) -> Self {
-        Self::new(bytes, k_type)
+impl From<(Vec<u8>)> for Key {
+    fn from((bytes): (Vec<u8>)) -> Self {
+        Self::new(bytes)
     }
 }
 
