d5ab69082616ae86747a3a3208c4081ca0df263d
andreas <Andreas.Zwander@gmail.com>
2026-04-30 21:51:15 +0200

    Simplify `Key` creation in `main.rs` by removing `KeyType` usage and updating constructors

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use OmniKey::key::{Key, KeyType};
+use OmniKey::key::{Key};
 use OmniKey::key_set::KeySet;
 use OmniKey::wireguard_key_set::WireGuardKeySet;
 
 fn main() -> std::io::Result<()> {
 
-    let public = Key::from((vec![1, 2, 3], KeyType::Public));
-    let private = Key::from((vec![4, 5, 6], KeyType::Private));
-    let preshared = Key::from((vec![7, 8, 9], KeyType::Preshared));
+    let public = Key::from(vec![1, 2, 3]);
+    let private = Key::from(vec![4, 5, 6]);
+    let preshared = Key::from(vec![7, 8, 9]);
     let keyset = KeySet::new(private.clone(), public.clone());
     println!("{keyset}");
     let wireguard_key_set = WireGuardKeySet::new(keyset.clone(), Option::from(preshared.clone()));
