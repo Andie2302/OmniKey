d3f20689d1620c5086f267e03a03961680a0e554
andreas <Andreas.Zwander@gmail.com>
2026-04-30 23:11:07 +0200

    Remove outdated comments in `main.rs` to improve code readability

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,9 @@ use OmniKey::wireguard_key_set::WireGuardKeySet;
 use OmniKey::ssh_key_set::SSHKeySet;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // WireGuard: X25519 Keypair + Preshared Key
     let wg = WireGuardKeySet::generate(true);
     println!("{wg}");
-
-    // SSH: Ed25519 Keypair im OpenSSH-Format
     let ssh = SSHKeySet::generate()?;
     println!("{ssh}");
-
     Ok(())
 }
