99847612b156e9f4be78d1103c44cfecde4068ed
andreas <Andreas.Zwander@gmail.com>
2026-04-30 23:10:54 +0200

    Remove unnecessary blank line in `KeySet` constructor for cleaner code

--- src/key_set.rs
+++ src/key_set.rs
@@ -12,7 +12,6 @@ impl KeySet {
     pub fn new(private_key: PrivateKey, public_key: PublicKey) -> Self {
         Self { private_key, public_key }
     }
-
     pub fn private_key(&self) -> &PrivateKey { &self.private_key }
     pub fn public_key(&self) -> &PublicKey   { &self.public_key  }
 }
