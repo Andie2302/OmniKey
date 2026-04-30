4b915dedc2b8b773a3cef4d1181b85ac1727d043
andreas <Andreas.Zwander@gmail.com>
2026-04-30 22:20:35 +0200

    Refactor `main.rs`: streamline key generation using `WireGuardKeySet` and `SSHKeySet`, update error handling

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
-use OmniKey::key_set::KeySet;
 use OmniKey::wireguard_key_set::WireGuardKeySet;
-use OmniKey::public_key::PublicKey;
-use OmniKey::preshared_key::PresharedKey;
-use OmniKey::private_key::PrivateKey;
+use OmniKey::ssh_key_set::SSHKeySet;
 
-fn main() -> std::io::Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // WireGuard: X25519 Keypair + Preshared Key
+    let wg = WireGuardKeySet::generate(true);
+    println!("{wg}");
 
-    let public = PublicKey::from(vec![1, 2, 3]);
-    let private = PrivateKey::from(vec![4, 5, 6]);
-    let preshared = PresharedKey::from(vec![7, 8, 9]);
-    let keyset = KeySet::new(private.clone(), public.clone());
-    println!("{keyset}");
-    let wireguard_key_set = WireGuardKeySet::new(keyset.clone(), Option::from(preshared.clone()));
-    println!("{wireguard_key_set}");
+    // SSH: Ed25519 Keypair im OpenSSH-Format
+    let ssh = SSHKeySet::generate()?;
+    println!("{ssh}");
+
     Ok(())
 }
