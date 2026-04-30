2ddcfd007e2d2b07f12787d329d647b4a2e013cb
andreas <Andreas.Zwander@gmail.com>
2026-04-30 21:32:59 +0200

    Update `main.rs` to use `KeyType` and enhance key handling clarity

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use OmniKey::key::Key;
+use OmniKey::key::{Key, KeyType};
 use OmniKey::key_set::KeySet;
 use OmniKey::wireguard_key_set::WireGuardKeySet;
 
 fn main() -> std::io::Result<()> {
 
-    let key1 = Key::from(vec![1, 2, 3]);
-    let key2 = Key::from(vec![4, 5, 6]);
-    let preshared = Key::from(vec![7, 8, 9]);
-    let keyset = KeySet::new(key1.clone(), key2.clone());
+    let public = Key::from((vec![1, 2, 3], KeyType::Public));
+    let private = Key::from((vec![4, 5, 6], KeyType::Private));
+    let preshared = Key::from((vec![7, 8, 9], KeyType::Preshared));
+    let keyset = KeySet::new(private.clone(), public.clone());
     println!("{keyset}");
     let wireguard_key_set = WireGuardKeySet::new(keyset.clone(), Option::from(preshared.clone()));
     println!("{wireguard_key_set}");
