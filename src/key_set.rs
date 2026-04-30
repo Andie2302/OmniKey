49f6f524c103896eff0e5790d47fb7f953ff0817
andreas <Andreas.Zwander@gmail.com>
2026-04-30 21:51:07 +0200

    Refactor `KeySet`: replace `Key` with `PrivateKey` and `PublicKey` for improved type clarity

--- src/key_set.rs
+++ src/key_set.rs
@@ -1,22 +1,23 @@
-use crate::key::Key;
 use std::fmt;
+use crate::private_key::PrivateKey;
+use crate::public_key::PublicKey;
 
 #[derive(Debug, Clone)]
 pub struct KeySet {
-    private_key: Key,
-    public_key: Key,
+    private_key: PrivateKey,
+    public_key: PublicKey,
 }
 
 impl KeySet {
-    pub fn new(private_key: Key, public_key: Key) -> Self {
+    pub fn new(private_key: PrivateKey, public_key: PublicKey) -> Self {
         Self {
             private_key,
             public_key,
         }
     }
 
-    pub fn private_key(&self) -> &Key { &self.private_key }
-    pub fn public_key(&self) -> &Key { &self.public_key }
+    pub fn private_key(&self) -> &PrivateKey { &self.private_key }
+    pub fn public_key(&self) -> &PublicKey { &self.public_key }
 }
 
 impl fmt::Display for KeySet {
