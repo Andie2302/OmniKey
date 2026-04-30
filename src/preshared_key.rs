3f659bcbba8d616cc8afbc1572dbe45f6d2605ad
andreas <Andreas.Zwander@gmail.com>
2026-04-30 22:20:39 +0200

    Enhance `PresharedKey`: add `as_bytes` and `to_base64` methods, refine `Display` formatting, and fix `From<Vec<u8>>` implementation

--- src/preshared_key.rs
+++ src/preshared_key.rs
@@ -10,17 +10,24 @@ impl PresharedKey {
     pub fn new(preshared_key: Key) -> Self {
         Self { preshared_key }
     }
+
+    pub fn as_bytes(&self) -> &[u8] {
+        self.preshared_key.as_bytes()
+    }
+
+    pub fn to_base64(&self) -> String {
+        self.preshared_key.to_base64()
+    }
 }
 
 impl fmt::Display for PresharedKey {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let value = self.preshared_key.to_base64();
-        write!(f, "  Value: {value}")
+        write!(f, "  Value: {}", self.preshared_key.to_base64())
     }
 }
 
-impl From<(Vec<u8>)> for PresharedKey {
-    fn from((bytes): (Vec<u8>)) -> Self {
+impl From<Vec<u8>> for PresharedKey {
+    fn from(bytes: Vec<u8>) -> Self {
         Self::new(Key::from(bytes))
     }
 }
