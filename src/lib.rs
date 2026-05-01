a0f58b065648b971c4db9da7cd1385668624cc45
andreas <Andreas.Zwander@gmail.com>
2026-05-01 02:42:37 +0200

    Reorganize module structure; add `error` and `writer` modules; consolidate and rename key-related modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
+pub mod error;
+pub mod generator;
 pub mod key;
 pub mod key_set;
-pub mod wireguard_key_set;
+pub mod keys;
 pub mod ssh_key_set;
-pub mod private_key;
-pub mod public_key;
-pub mod preshared_key;
-pub mod generator;
+pub mod wireguard_key_set;
+pub mod writer;
 mod displays;
