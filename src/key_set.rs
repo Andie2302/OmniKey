06d17b3d942e58db392d8664cf8f54764b64b1c1
andreas <Andreas.Zwander@gmail.com>
2026-05-01 02:42:32 +0200

    Refactor `KeySet` struct to use grouped `keys` module imports; add `#[must_use]` annotations for better API usage guidance

--- src/key_set.rs
+++ src/key_set.rs
@@ -1,6 +1,6 @@
-use crate::private_key::PrivateKey;
-use crate::public_key::PublicKey;
+use crate::keys::{PrivateKey, PublicKey};
 
+/// A matched private/public key pair.
 #[derive(Debug, Clone)]
 pub struct KeySet {
     private_key: PrivateKey,
@@ -8,9 +8,18 @@ pub struct KeySet {
 }
 
 impl KeySet {
+    #[must_use]
     pub fn new(private_key: PrivateKey, public_key: PublicKey) -> Self {
         Self { private_key, public_key }
     }
-    pub fn private_key(&self) -> &PrivateKey { &self.private_key }
-    pub fn public_key(&self) -> &PublicKey   { &self.public_key  }
+
+    #[must_use]
+    pub fn private_key(&self) -> &PrivateKey {
+        &self.private_key
+    }
+
+    #[must_use]
+    pub fn public_key(&self) -> &PublicKey {
+        &self.public_key
+    }
 }
