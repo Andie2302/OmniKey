a50529fa70225e5b47f5a62b5966b17bd7fdc5da
andreas <Andreas.Zwander@gmail.com>
2026-05-01 02:42:58 +0200

    Refactor `WireGuardKeySet` to use grouped `keys` module imports; add `#[must_use]` annotations to accessor methods for API clarity

--- src/wireguard_key_set.rs
+++ src/wireguard_key_set.rs
@@ -1,8 +1,7 @@
 use crate::key_set::KeySet;
-use crate::preshared_key::PresharedKey;
-use crate::private_key::PrivateKey;
-use crate::public_key::PublicKey;
+use crate::keys::{PresharedKey, PrivateKey, PublicKey};
 
+/// An X25519 WireGuard key pair plus an optional pre-shared key.
 #[derive(Debug, Clone)]
 pub struct WireGuardKeySet {
     keyset: KeySet,
@@ -10,10 +9,23 @@ pub struct WireGuardKeySet {
 }
 
 impl WireGuardKeySet {
+    #[must_use]
     pub fn new(keyset: KeySet, preshared_key: Option<PresharedKey>) -> Self {
         Self { keyset, preshared_key }
     }
-    pub fn private_key(&self) -> &PrivateKey         { self.keyset.private_key() }
-    pub fn public_key(&self)  -> &PublicKey          { self.keyset.public_key()  }
-    pub fn preshared_key(&self) -> Option<&PresharedKey> { self.preshared_key.as_ref() }
+
+    #[must_use]
+    pub fn private_key(&self) -> &PrivateKey {
+        self.keyset.private_key()
+    }
+
+    #[must_use]
+    pub fn public_key(&self) -> &PublicKey {
+        self.keyset.public_key()
+    }
+
+    #[must_use]
+    pub fn preshared_key(&self) -> Option<&PresharedKey> {
+        self.preshared_key.as_ref()
+    }
 }
